09094dd2856efbd613bb64607bd89cf8bb2eabb2
Wes Morgan <git@wesmorgan.me>
2021-07-26 12:59:25 -0600

    Create instance inside go block

--- src/gdfb/core.clj
+++ src/gdfb/core.clj
@@ -3,12 +3,16 @@
   (:gen-class))
 
 
+(set! *warn-on-reflection* true)
+
+
 (deftype Thingy [a b c])
 
 
 (defn -main [& _]
-  (let [thing (->Thingy "a" "b" "c")]
+  (let [^Thingy thing (->Thingy "a" "b" "c")]
     (println "Outside the go block:" (.-a thing)) ; this works on regular JVM & graalvm native-image
     (go
-      (println "Inside the go block:" (.-a thing)))) ; this works on regular JVM but blows up on graalvm native-image
+      (let [^Thingy other (->Thingy "1" "2" "3")]
+        (println "Inside the go block:" (.-a other))))) ; this works on regular JVM but blows up on graalvm native-image
   (Thread/sleep 2000))
